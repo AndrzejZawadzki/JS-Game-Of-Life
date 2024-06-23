7222ecce54887dacf656fa8830aeb1c0287c2ad7
AndrzejZawadzki <andrzejzawadzki@o2.pl>
2024-06-23 19:07:15 +0200

    Created the board component

--- src/app/game-of-life/game-of-life.component.ts
+++ src/app/game-of-life/game-of-life.component.ts
@@ -12,6 +12,7 @@ export class GameOfLifeComponent implements OnInit {
   rows: number = 20;
   cols: number = 30;
   board: boolean[][] = [];
+  intervalId: any;
 
   ngOnInit(): void {
     this.initializedBoard();
@@ -26,4 +27,19 @@ export class GameOfLifeComponent implements OnInit {
   toggleCell(row: number, col: number): void {
     this.board[row][col] = !this.board[row][col];
   }
+
+  startGame(): void {
+    this.intervalId = setInterval(() => this.nextGeneration(), 100);
+  }
+
+  stopGame(): void {
+    clearInterval(this.intervalId);
+  }
+
+  resetGame(): void {
+    this.stopGame();
+    this.initializedBoard();
+  }
+
+  nextGeneration(): void {}
 }
