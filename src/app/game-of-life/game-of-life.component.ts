55891a5c5f298bc66a593038bf508c9b392d293a
AndrzejZawadzki <andrzejzawadzki@o2.pl>
2024-06-24 21:09:10 +0200

    Small improvements

--- src/app/game-of-life/game-of-life.component.ts
+++ src/app/game-of-life/game-of-life.component.ts
@@ -9,27 +9,27 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game-of-life.component.css',
 })
 export class GameOfLifeComponent implements OnInit {
-  rows: number = 20;
-  cols: number = 30;
+  numberOfRows: number = 10;
+  numberOfColumns: number = 25;
   board: boolean[][] = [];
-  intervalId: any;
+  intervalId: any = 1000;
 
   ngOnInit(): void {
     this.initializedBoard();
   }
-
+  // Initialize the board with all cells set to false (dead)
   initializedBoard(): void {
-    this.board = Array.from({ length: this.rows }, () =>
-      Array(this.cols).fill(false)
+    this.board = Array.from({ length: this.numberOfRows }, () =>
+      Array(this.numberOfColumns).fill(false)
     );
   }
-
+  // Toggle the state of a cell (alive or dead)
   toggleCell(row: number, col: number): void {
     this.board[row][col] = !this.board[row][col];
   }
 
   startGame(): void {
-    this.intervalId = setInterval(() => this.nextGeneration(), 100);
+    this.intervalId = setInterval(() => this.nextGeneration(), this.intervalId);
   }
 
   stopGame(): void {
@@ -41,5 +41,58 @@ export class GameOfLifeComponent implements OnInit {
     this.initializedBoard();
   }
 
-  nextGeneration(): void {}
+  nextGeneration(): void {
+    const newBoard = this.board.map((arr) => [...arr]);
+    let changes = false;
+
+    for (let row = 0; row < this.numberOfRows; row++) {
+      for (let col = 0; col < this.numberOfColumns; col++) {
+        const aliveNeighbors = this.countAliveNeighbors(row, col);
+
+        if (this.board[row][col]) {
+          if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+            newBoard[row][col] = false;
+            changes = true;
+          }
+        } else {
+          if (aliveNeighbors === 3) {
+            newBoard[row][col] = true;
+            changes = true;
+          }
+        }
+      }
+    }
+
+    this.board = newBoard;
+
+    if (!changes) {
+      this.stopGame();
+    }
+  }
+
+  countAliveNeighbors(row: number, col: number): number {
+    let numberOfAliveNighbors = 0;
+
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i === 0 && j === 0) continue;
+
+        const newRow = row + i;
+        const newCol = col + j;
+
+        if (
+          newRow >= 0 &&
+          newRow < this.numberOfRows &&
+          newCol >= 0 &&
+          newCol < this.numberOfColumns
+        ) {
+          if (this.board[newRow][newCol]) {
+            numberOfAliveNighbors++;
+          }
+        }
+      }
+    }
+    console.log('numberOfAliveNighbors: ', numberOfAliveNighbors);
+    return numberOfAliveNighbors;
+  }
 }
